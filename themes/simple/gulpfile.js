d751617ea5bbc33e4efcd38e631445ae09925ef6
Kira McLean <kiraemclean@gmail.com>
2016-04-28 19:37:41 -0400

    gulpfile can't handle string of paths

--- themes/simple/gulpfile.js
+++ themes/simple/gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('./sass/*.sass', './sass/*.scss')
+  return gulp.src('./sass/*')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./static/css'));
+    .pipe(gulp.dest('./static/css/'));
 });
 
 gulp.task('sass:watch', function() {
