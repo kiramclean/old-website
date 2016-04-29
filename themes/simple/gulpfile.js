2a02b406f6d03361e4f5e63ae0212114e1240b1d
Kira McLean <kiraemclean@gmail.com>
2016-04-28 23:52:16 -0400

    update layout and styles

--- themes/simple/gulpfile.js
+++ themes/simple/gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var input = './sass/*';
+var output = './static/css/';
+
 gulp.task('sass', function() {
-  return gulp.src('./sass/*')
+  return gulp.src(input)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./static/css/'));
+    .pipe(gulp.dest(output));
 });
 
 gulp.task('sass:watch', function() {
