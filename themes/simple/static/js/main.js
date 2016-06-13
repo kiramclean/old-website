264cf57ab4f3e243b4ba7e498f23e7b3b83f8a82
Kira McLean <kiraemclean@gmail.com>
2016-06-12 21:35:38 -0400

    another attempt at hover issue

--- themes/simple/static/js/main.js
+++ themes/simple/static/js/main.js
@@ -1,18 +1,18 @@
-// $(document).ready(function() {
-//    $('a').on('click touchend', function(e) {
-//       var el = $(this);
-//       var link = el.attr('href');
-//       window.location = link;
-//    });
-// });
-//
-//
 $(document).ready(function() {
-    $('.hover').bind('touchstart touchend', function(e) {
-        e.preventDefault();
-        var element = $(this);
-        element.toggleClass('hover_effect');
-        var link = element.attr('href');
-        window.location = link;
-    });
+   $('.slide').on('click touchend', function(e) {
+      var el = $(this);
+      var link = el.attr('href');
+      el.toggleClass('hover_effect');
+      window.location = link;
+   });
 });
+
+// $(document).ready(function() {
+//     $('.hover').bind('touchstart touchend', function(e) {
+//         e.preventDefault();
+//         var element = $(this);
+//         element.toggleClass('hover_effect');
+//         var link = element.attr('href');
+//         window.location = link;
+//     });
+// });
